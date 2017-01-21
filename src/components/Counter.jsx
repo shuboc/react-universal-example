e2317f5bb5d38f04c9b16ae5e3561d20ac4b1002
Shubo Chao <apiggy0204@gmail.com>
2017-01-21 20:15:49 +0800

    Rename reducer

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import * as actions from '../store'
+import * as actions from '../store/count'
 
 const Counter = ({count, add, subtract}) => (
   <div>
