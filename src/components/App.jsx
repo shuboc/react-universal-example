d1f0bd11a9e903a93a35c111afd971f40ed4efe6
Shubo Chao <apiggy0204@gmail.com>
2017-01-30 20:41:41 +0800

    Use query params

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,25 @@
 import React, {PropTypes} from 'react'
 import {Link} from 'react-router'
+import {connect} from 'react-redux'
 
-const App = ({children}) => (
+const App = ({children, query}) => (
   <div>
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to={{pathname: '/', query}}>Home</Link>
+      <Link to={{pathname: '/about', query}}>About</Link>
+      <div>User: {query.user || 'Anonymous'}</div>
     </div>
     {children}
   </div>
 )
 
 App.propTypes = {
-  children: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
+  query: PropTypes.object
 }
 
-export default App
+const mapStateToProps = (state, ownProps) => ({
+  query: ownProps.location.query
+})
+
+export default connect(mapStateToProps)(App)
